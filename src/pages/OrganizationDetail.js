2d4512a8313a75c154651e5de10e3149f8148faf
haris-315 <hkffking@gmail.com>
2026-08-31 15:06:11 +0500

    Update OrganizationDetail

--- src/pages/OrganizationDetail.js
+++ src/pages/OrganizationDetail.js
@@ -43,7 +43,7 @@ export const OrganizationDetail = () => {
     useEffect(() => {
         if (selectedOrganization) {
             setFormData({
-                name: selectedOrganization.name || '',
+                nameecle: selectedOrganization.name || '',
                 industry: selectedOrganization.industry || '',
                 headcount: selectedOrganization.headcount || '',
                 annual_revenue: selectedOrganization.annual_revenue || '',
